d06d6503ea12c39e4beaf1cb6a448386ed92f84f
Andrei Chumakou <chumakou.andrei@gmail.com>
2023-03-13 08:33:56 +0300

    refactor TestsExemples.java add testTwo() method

--- src/test/java/testsRestAssured/TestsExemples.java
+++ src/test/java/testsRestAssured/TestsExemples.java
@@ -1,16 +1,24 @@
 package testsRestAssured;
 
-import io.restassured.RestAssured;
-import io.restassured.response.Response;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.*;
+import io.restassured.response.Response;
+import static io.restassured.matcher.RestAssuredMatchers.*;
+import static org.hamcrest.Matchers.*;
+
+//import org.hamcrest.Matcher;
+//import static io.restassured.RestAssured.given;
+//import static org.apache.commons.codec.digest.UnixCrypt.body;
+//import static org.hamcrest.Matchers.equalTo;
 
 public class TestsExemples {
 
     @Test
     public void testOne() {
         String path = "https://reqres.in/api/users?page=2";
-        Response response = RestAssured.get(path);
+        Response response = get(path);
 
         System.out.println(response.getStatusCode());
         System.out.println(response.getTime());
@@ -21,4 +29,18 @@ public class TestsExemples {
         int statusCode = response.getStatusCode();
         Assert.assertEquals(statusCode, 200);
     }
+
+    @Test
+    public void testTwo() {
+        baseURI = "https://reqres.in/api";
+        given()
+                .get("/users?page=2")
+                .then().statusCode(200)
+                .body("data[1].id", equalTo(8)).log().all();
+        given()
+                .get("/users?page=2")
+                .then().statusCode(200)
+                .log().all();
+    }
+
 }
